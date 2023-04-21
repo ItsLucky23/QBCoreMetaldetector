9c9febd2a401a0f4648067c688c654fe06d6fd73
Lucky23 <111248859+ItsLucky23@users.noreply.github.com>
2023-04-21 18:15:39 +0200

    Add files via upload

---
+++ qbcore_metaldetector/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version "adamant"
+game "gta5"
+
+dependencies {
+    "qb-core"
+}
+
+client_scripts {
+    "client/*.lua"
+}
+
+
+server_scripts {
+    'server/*.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+file 'stream/gen_w_am_metaldetector.ytyp'
